e59b4331c027a789c83fd61cdb8c9830801a5461
MUHMMADSALEH <khanmohdsaleh2002@gmail.com>
2024-11-14 22:29:43 +0530

    feat:add Hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={clsx(dmSans.className, "antialiased bg-black ")}>{children}</body>
+      <body className={clsx(dmSans.className, "antialiased  ")}>{children}</body>
     </html>
   );
 }
