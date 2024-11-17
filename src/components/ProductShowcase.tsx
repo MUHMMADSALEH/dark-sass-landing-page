6deb41babcf4d89e167f417db3c1f8bf92daebfd
MUHMMADSALEH <khanmohdsaleh2002@gmail.com>
2024-11-17 11:12:17 +0530

    feat:add animation on ProductShowCase section

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,9 +1,22 @@
-
+"use client"
 import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
+import {motion,useScroll, useTransform} from "framer-motion"
+import { useEffect, useRef } from "react";
 
 
 export const ProductShowcase = () => {
+    const appImage=useRef<HTMLImageElement>(null)
+    const {scrollYProgress}=useScroll({
+        target:appImage,
+        offset:["start end","end end"]
+    })
+    useEffect(()=>{
+        scrollYProgress.on("change",(latestValue)=>console.log("latestValue",latestValue))
+    },[])
+
+    const rotateX=useTransform(scrollYProgress,[0,1],[15,0])
+    const opacity=useTransform(scrollYProgress,[0,1],[.5,1])
 return (
 <div className="bg-black text-white bg-gradient-to-b ☐ from-black to-[#5D2CA8] py-[72px] sm:py-24">
 <div className="container">
@@ -16,7 +29,12 @@ task at a time.
 </p>
 </div>
 <div className="w-full">
-<Image src={appScreen} alt="The product screenshot" className="mt-14 lg:mx-auto" />
+<motion.div style={{opacity:opacity,
+    rotateX:rotateX,
+    transformPerspective:"800px"
+}}>
+<Image src={appScreen} alt="The product screenshot" className="mt-14 lg:mx-auto" ref={appImage}/>
+</motion.div>
 </div>
 </div>
 </div>
